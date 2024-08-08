90e91087f15a747ffd0946c60e2abe8a91ddc7b1
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-08 11:24:08 -0700

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-// import Tilt from "react-tilt";
+import {Tilt} from 'react-tilt'; 
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
-import {fadeIn,textVariant} from '../utils/motion'
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
+const ServiceCard = ({index,title,icon}) => {
+  return (
+    <Tilt className="w-full xs:w-[250px]">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div options={{
+            max: 45,
+            scale:1,
+            speed: 450
+        }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center' >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] text-center font-bold'>{ title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 const About = () => {
   return (
     <>
@@ -20,9 +40,13 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div></div>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
 
-export default About
+export default SectionWrapper(About,"about")

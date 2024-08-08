29e29a696c97ad1bfcd72d74f91003f853fdde84
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-08 10:02:32 -0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion"
+
+import { styles } from "../styles"
+import {ComputersCanvas} from './canvas'
+const Hero = () => {
+  return (
+    <section className="realtive w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Bement</span></h1>
+          <p className={`${styles.heroSubText}`}>
+            I develop 3D visuals ,user <br className="sm:block hidden" />        interfaces and web application
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="flex justify-center items-center w-full absolute bottom-32 xs:bottom-10">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary p-2 flex justify-center items-start">
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className="bg-secondary w-3 h-3 rounded-full mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+} 
+
+export default Hero
